00b4f934b20b7f315c94d79f630af0246bd28304
Mariya <impa1002@gmail.com>
2023-12-13 15:09:44 +0300

    добавлена папка дз урок 2

---
+++ EX_2/script.js
@@ -0,0 +1,7 @@
+
+let name = "Mariya";
+let admin = name;
+console.log(`Значение переменной admin = ${admin}`)
+
+let C = 55;
+console.log(`Температура в цельсиях ${C} = Температура в фаренгейтах ${(9 / 5) * C + 32}`)
